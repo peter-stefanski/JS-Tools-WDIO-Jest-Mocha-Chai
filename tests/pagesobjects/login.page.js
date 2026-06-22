ed84b7c0ac053b104666c6170bbb8c77d41ca8e0
Piotr Stefanski <peterstefanski3@gmail.com>
2026-06-22 14:02:16 +0200

    Moved all selectors into POM file

--- tests/pagesobjects/login.page.js
+++ tests/pagesobjects/login.page.js
@@ -10,6 +10,15 @@ class LoginPage {
   async getHeadingText() {
     return this.heading.getText();
   }
-}
 
+  get loginForm() {
+    return {
+      cookieButton: $("button.fc-cta-consent"),
+      emailInput: $('input[data-qa="login-email"]'),
+      passwordInput: $('input[data-qa="login-password"]'),
+      submitLoginButton: $('button[data-qa="login-button"]'),
+      errorMessage: $(".login-form p"),
+    };
+  }
+}
 module.exports = new LoginPage();
--- tests/specs/negative_login.spec.js
+++ tests/specs/negative_login.spec.js
@@ -1,23 +1,17 @@
-import { remote } from "webdriverio";
+const LoginPage = require("../pagesobjects/login.page");
 
 describe("Negative test of Login", () => {
   it("Should display an error message for invalid credentials", async () => {
-    await browser.url("/login");
+    await LoginPage.open();
 
-    const cookieButton = await $("button.fc-cta-consent");
-    const emailInput = await $('input[data-qa="login-email"]');
-    const passwordInput = await $('input[data-qa="login-password"]');
-    const submitLoginButton = await $('button[data-qa="login-button"]');
-    const errorMessage = await $(".login-form p");
-
-    await cookieButton.waitForClickable();
-    await cookieButton.click();
-    await emailInput.waitForClickable();
-    await emailInput.setValue("test@test.com");
-    await passwordInput.setValue("examplePassword");
-    await submitLoginButton.click();
-    await errorMessage.waitForDisplayed();
-    await expect(errorMessage).toHaveText(
+    await LoginPage.loginForm.cookieButton.waitForClickable();
+    await LoginPage.loginForm.cookieButton.click();
+    await LoginPage.loginForm.emailInput.waitForClickable();
+    await LoginPage.loginForm.emailInput.setValue("test@test.com");
+    await LoginPage.loginForm.passwordInput.setValue("examplePassword");
+    await LoginPage.loginForm.submitLoginButton.click();
+    await LoginPage.loginForm.errorMessage.waitForDisplayed();
+    await expect(LoginPage.loginForm.errorMessage).toHaveText(
       "Your email or password is incorrect!",
     );
   });
